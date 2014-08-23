5d78db89f4b60ac935a041b8d6679b0f7d3fb4aa
Tiziano Lattisi <tiziano@axiastudio.it>
2014-08-23 14:20:35 +0200

    FamFamFam (http://www.famfamfam.com/lab/icons/silk/) theme.

---
+++ src/com/axiastudio/zoefx/core/skins/FamFamFam.java
@@ -0,0 +1,19 @@
+package com.axiastudio.zoefx.core.skins;
+
+/**
+ * User: tiziano
+ * Date: 23/08/14
+ * Time: 10:46
+ */
+public class FamFamFam implements ZSkin {
+
+    @Override
+    public String resourcesFolder() {
+        return "/com/axiastudio/zoefx/core/skins/famfamfam/";
+    }
+
+    @Override
+    public String getName() {
+        return "famfamfam";
+    }
+}
